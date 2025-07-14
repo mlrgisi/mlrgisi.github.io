e32223eb97abe6928f1df4fa8f22e2e9a4f0b3c3
Your Name <you@example.com>
2025-07-14 21:16:44 +0530

    External Link Warning Test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -87,7 +87,6 @@ export function Footer() {
             </p>
             <div className="flex space-x-4">
               {socialLinks.map((link) => (
-                {/**************
                 <motion.a
                   key={link.label}
                   href={link.href}
@@ -98,19 +97,6 @@ export function Footer() {
                 >
                   <link.icon className="w-5 h-5" />
                 </motion.a> 
-                ***************/}
-                <ExternalLinkWrapper
-                  key={link.label}
-                  href={link.href}
-                  className="p-2 bg-gray-800 rounded-lg hover:bg-gray-700 transition-colors"
-                >
-                  <motion.div
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                  >
-                    <link.icon className="w-5 h-5" />
-                  </motion.div>
-                </ExternalLinkWrapper>
               ))}
             </div>
           </div>
